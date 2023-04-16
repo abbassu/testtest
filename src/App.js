a0951c2ee406ed9fc0c65d0f3716c2144565f4a6
abbassu <abbassurakji99@gmail.com>
2023-04-16 11:56:49 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<h1>
+          This work is for applying for GSG training 
+          </h1>
+          <hr />
+          <div>
+            <h2> E-Commerce Project  </h2>
+            <h3> Technology : React,Redux,scss,html </h3>
+            <p> this is last project i build it you can sign in as admin --- using email : adminadmin@gmail.com and password : 98745632159 </p>
+            <span>
+              <a href="https://abbasclothing.netlify.app/"> Online </a>
+              <br />
+              <a href="https://github.com/abbassu/Clothing-Project.git"> Github </a>
+            </span>
+            
+          </div>
+          <hr />
+          <div>
+            <h2> Food Project  </h2>
+            <h3> Technology : React,scss,html </h3>
+            <span>
+              <a href="https://github.com/abbassu/Food-Recipes.git"> Readme on Github </a>
+            </span>
+            
+          </div>
+          <hr />
+
+          <div>
+            <h2> Climb Page  </h2>
+            <h3> Technology : html,css </h3>
+            <span>
+              <a href="https://abbassu.github.io/Html-and-Css-Template-One/"> Online </a>
+              <br />
+              <a href="https://github.com/abbassu/Html-and-Css-Template-One.git"> Github</a>
+            </span>
+            
+          </div>
+          <hr />
+
+
+
+    </div>
+  );
+}
+
+export default App;
